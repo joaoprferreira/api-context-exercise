af59de7b539ceeff4147884994802b63efa7506b
João Pedro <joaof082@gmail.com>
2021-07-07 22:13:34 -0300

    eslios da main
    Signed-off-by: João Pedro <joaof082@gmail.com>

---
+++ src/Components/Main/MainStyled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import "../../assets/fonts/font.css";
+
+export const MainComponent = styled.main `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80vh;
+  border-radius: 20px;
+  font-family: 'Roboto Mono', monospace;
+
+  .button-main {
+    width: 100px;
+    height: 80px;
+    background-color:#BF0A2B;
+    border-radius: 20px;
+    border: none;
+    color: white;
+  }
+`
