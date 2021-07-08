ee61c72dcb7b966a30760f2842682e2a94e08e8a
João Pedro <joaof082@gmail.com>
2021-07-07 22:13:55 -0300

    Criação do contexto para o contado
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- src/providers/Context.js
+++ src/providers/Context.js
@@ -1,22 +1,25 @@
-import React, { createContext, useState }  from "react";
+import React, { createContext, useState, useContext  }  from "react";
 
-export const CounterContext = createContext()
+const CounterContext = createContext()
 
 export function CounterProvider({children}) {
   const [ counter, setCounter ] = useState(0)
 
   return (
-    <CounterContext.Provider value={{
-      counter,
-      setCounter
-    }}>
+    <CounterContext.Provider 
+      value={{
+        counter,
+        setCounter
+      }}
+    >
       {children}
     </CounterContext.Provider>
   )
 }
-/* 
+
 export function useCount() {
   const context = useContext(CounterContext)
-  const [ conuter, setCounter ] = context;
-  return context;
-} */
+  if ( !context ) throw new Error(" useCount must be used within a counterProvider  ")
+  const { counter, setCounter } = context;
+  return { counter, setCounter };
+}
